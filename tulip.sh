8063e630a9fdb3643e0afa673f1f552dc7ab5f6a
ieprst <33570058+ieprst@users.noreply.github.com>
2019-02-24 12:12:25 +0300

    add tulip.sh

---
+++ tulip.sh
@@ -0,0 +1,9 @@
+export USE_CCACHE=1
+export CCACHE_COMPRESS=1 
+export CCACHE_DIR=/home/$USER/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 50G
+repo sync -f --force-sync --no-clone-bundle -j8
+. build/envsetup.sh; 
+lunch lineage_tulip-userdebug
+make clean 
+brunch lineage_tulip-userdebug -j4﻿
